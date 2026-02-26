235aa4a3d7b56709a95db0ab23e2da46a812cbac
Ivan Ronaldo <ronaldoneira10@gmail.com>
2026-02-25 21:59:52 -0500

    subida de datos seguir estudiando

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
+require('dotenv').config();
 const { configDotenv } = require('dotenv');
 const express = require('express');
+const cors = require('cors');
 const { CLIENT_RENEG_LIMIT } = require('tls');
 const morgan = requiere('morgan');
-require('dotenv').config();
-
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const { version } = require('os');
+const specs = swaggerJssDoc({
+    definition: {
+        openpi: "3.0.0",
+        info: {
+             title: "MARKETPLACE API", version: "1.0.0"},
+        },
+        apis: ["./routes/*.js"],
+    });
 const app = express();
+const brcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql2/promise');
 // Middleware
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 
 const PORT = process.env.PORT || 3000;
